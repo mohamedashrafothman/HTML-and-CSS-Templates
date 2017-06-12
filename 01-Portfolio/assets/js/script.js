cdd1b3825ed714a2d386283df9beb803b44523d2
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2017-06-12 05:44:25 +0200

    project one (finished)
    project one (finished)

--- 01-Portfolio/assets/js/script.js
+++ 01-Portfolio/assets/js/script.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
+// Loading Screen
+$(window).on('load',function() {
+	var counter = 0;
+  	var c = 0;
+  	var i = setInterval(function(){
+  		
+    	$("#loading-page .counter h1").html(c + "%");
+    	counter++;
+    	c++;
+     	if(counter == 101) {
+        	clearInterval(i);
+        	$("#loading-page").animate({'opacity':'0'}, 1000, function(){
+        		$(this).css({'display':'none'});
+        	});
+    	}
+  	}, 50);
+});
 
+
+
+$(document).ready(function(){
 
 	$('#enterName').on('keydown', function(e){
 		$value = $(this).val();
@@ -128,4 +147,7 @@ $(document).ready(function(){
 
 	};
 	slider.init(true); // set automatic by True to enable automatic slideshow
+
+	
+
 });
