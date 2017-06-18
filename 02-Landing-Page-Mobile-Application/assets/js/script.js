7ec942d092bcd4e691ef5351f794e8f12a15225b
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2017-06-18 03:53:22 +0200

    working on project two (almost done 80% completed not responsive)
    working on project two (almost done 80% completed not responsive)

--- 02-Landing-Page-Mobile-Application/assets/js/script.js
+++ 02-Landing-Page-Mobile-Application/assets/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	// NavBar Scroll Effect
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 20){
-			$('#nav').addClass('active');
+			$('#nav').addClass('header-active');
 		} else {
-			$('#nav').removeClass('active');
+			$('#nav').removeClass('header-active');
 		}
 	});
 
@@ -22,46 +22,69 @@ $(document).ready(function(){
 		}
 	});
 
+	// Price Table
+	var individual_price_table = $('#price_tables').find('.individual');
+	var company_price_table = $('#price_tables').find('.company');
 
+
+	$('.switch-toggles').find('.individual').addClass('active');
+	$('#price_tables').find('.individual').addClass('active');
+
+	$('.switch-toggles').find('.individual').on('click', function(){
+		$(this).addClass('active');
+		$(this).closest('.switch-toggles').removeClass('active');
+		$(this).siblings().removeClass('active');
+		individual_price_table.addClass('active');
+		company_price_table.removeClass('active');
+	});
+
+	$('.switch-toggles').find('.company').on('click', function(){
+		$(this).addClass('active');
+		$(this).closest('.switch-toggles').addClass('active');
+		$(this).siblings().removeClass('active');
+		company_price_table.addClass('active');
+		individual_price_table.removeClass('active');
+	});
 		/*
 			===================== Scroll Smothy =====================
 		*/
-		// // Select all links with hashes
-		// $('a[href*="#"]')
-		// 	// Remove links that don't actually link to anything
-		// 	.not('[href="#"]')
-		// 	.not('[href="#0"]')
-		// 	.click(function(event) {
-		// 	// On-page links
-		// 	if (
-		// 	  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-		// 	  &&
-		// 	  location.hostname == this.hostname
-		// 	) {
-		// 	  // Figure out element to scroll to
-		// 	  var target = $(this.hash);
-		// 	  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		// 	  // Does a scroll target exist?
-		// 	  if (target.length) {
-		// 	    // Only prevent default if animation is actually gonna happen
-		// 	    event.preventDefault();
-		// 	    $('html, body').animate({
-		// 	      scrollTop: target.offset().top
-		// 	    }, 1000, function() {
-		// 	      // Callback after animation
-		// 	      // Must change focus!
-		// 	      var $target = $(target);
-		// 	      $target.focus();
-		// 	      if ($target.is(":focus")) { // Checking if the target was focused
-		// 	        return false;
-		// 	      } else {
-		// 	        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-		// 	        $target.focus(); // Set focus again
-		// 	      };
-		// 	    });
-		// 	  }
-		// 	}
-		// });
+		// Select all links with hashes
+		$('a[href*="#"]')
+			// Remove links that don't actually link to anything
+			.not('[href="#"]')
+			.not('[href="#0"]')
+			.not('[href="#quote-carousel"]')
+			.click(function(event) {
+			// On-page links
+			if (
+			  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+			  &&
+			  location.hostname == this.hostname
+			) {
+			  // Figure out element to scroll to
+			  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			  // Does a scroll target exist?
+			  if (target.length) {
+			    // Only prevent default if animation is actually gonna happen
+			    event.preventDefault();
+			    $('html, body').animate({
+			      scrollTop: target.offset().top
+			    }, 1000, function() {
+			      // Callback after animation
+			      // Must change focus!
+			      var $target = $(target);
+			      $target.focus();
+			      if ($target.is(":focus")) { // Checking if the target was focused
+			        return false;
+			      } else {
+			        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+			        $target.focus(); // Set focus again
+			      };
+			    });
+			  }
+			}
+		});
 
 	// Wow Animations
 	wow = new WOW(
