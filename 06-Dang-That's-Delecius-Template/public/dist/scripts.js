b2a6ae1dfb08a1a0b3596f9f870203d25e177d18
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2017-09-19 21:21:28 +0200

    login, register, account and hearted pages added

--- 06-Dang-That's-Delecius-Template/public/dist/scripts.js
+++ 06-Dang-That's-Delecius-Template/public/dist/scripts.js
@@ -5320,5 +5320,37 @@
 });
 
 $(document).ready(function(){
-    
+	/*
+        ===================== Scroll Smothy =====================
+    */
+	// Select all links with hashes
+	$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.click(function (e) {
+			// On-page links
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				// Figure out element to scroll to
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				// Does a scroll target exist?
+				if (target.length) {
+					// Only prevent default if animation is actually gonna happen
+					e.preventDefault();
+					$('html, body').animate({scrollTop: target.offset().top}, 1000, function () {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(':focus')) { // Checking if the target was focused
+							return false;
+						} else {
+							$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						}
+					});
+				}
+			}
+		});
+
 });
